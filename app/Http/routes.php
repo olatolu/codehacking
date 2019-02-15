f53bccd3552e244a52a416d377807cae1f2460ac
olatolu <oluolateru@gmail.com>
2019-02-15 13:28:33 +0100

    Admin master page set up succesfully

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,20 @@
 |
 */
 
+use App\User;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::resource('/admin/users', 'AdminUsersController');
+
+Route::get('/admin', function (){
+
+   return view('admin.index');
+
 });
